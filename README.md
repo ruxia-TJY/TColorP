7540ecbef9e5a1f1c63c685ddd74d54a18b3f2c5
ruxia-TJY <ruxia.tjy@qq.com>
2022-04-07 11:16:33 +0800

    can change style

--- README.md
+++ README.md
@@ -21,6 +21,41 @@ print('TColorP')
 
 ![view](View.png)
 
+## how to use
+```python
+from TColorP import TColorP, TColor
+
+tcp = TColorP()
+
+# print success info 
+tcp.success('success')
+# print error info
+tcp.error('error')
+# print warning info
+tcp.warning('warning')
+# print normal info
+tcp.normal('normal')
+
+# print normal Style
+print(tcp.normalStyle)
+
+# change normal style
+tcp.normalStyle = [TColor.method_highlight,TColor.foreground_red]
+tcp.normal("normal")
+
+# change normal style once
+tcp.normal('normal once',style=[TColor.method_highlight,TColor.foreground_red])
+
+# format color
+tcp.formatColor([TColor.method_normal,TColor.foreground_red])
+print('this will change color with red')
+
+# print suppos method and color
+print(TColor())
+# method_normal=0,method_highlight=1,method_underline=4,method_reverse=7
+# foreground_black=30,foreground_red=31,foreground_green=32,foreground_yellow=33,foreground_blue=34,foreground_fuchsia=35,foreground_cyan=36,foreground_white=37
+# background_black=40,background_red=41,background_green=42,background_yellow=43,background_blue=44,background_fuchsia=45,background_cyan=46,background_white=47
+```
 
 ## License
 
--- TColorP/TColorP.py
+++ TColorP/TColorP.py
@@ -1,11 +1,16 @@
 '''
-    TColorP.py: 彩色输出
-
+    TColorP.py
+    print with color.
+    class TColor and TColorP
 '''
+
 class TColor:
+    '''
+        supposed Color and method.
+    '''
     method_normal=0
     method_highlight=1
-    method_undersore=4
+    method_underline=4
     method_reverse=7
 
     foreground_black=30
@@ -26,13 +31,28 @@ class TColor:
     background_cyan=46
     background_white=47
 
+    def __str__(self):
+        return f'''method_normal=0,method_highlight=1,method_underline=4,method_reverse=7
+foreground_black=30,foreground_red=31,foreground_green=32,foreground_yellow=33,foreground_blue=34,foreground_fuchsia=35,foreground_cyan=36,foreground_white=37
+background_black=40,background_red=41,background_green=42,background_yellow=43,background_blue=44,background_fuchsia=45,background_cyan=46,background_white=47
+'''
+
 class TColorP:
     def __init__(self):
-        pass
+        self.normalStyle = [TColor.method_normal]
+        self.successStyle = [TColor.method_normal,TColor.foreground_green]
+        self.warningStyle = [TColor.method_normal,TColor.foreground_yellow]
+        self.errorStyle = [TColor.method_normal,TColor.foreground_red]
 
-    def formatColor(self,style:list):
+    def formatColor(self,style:list) -> None:
+        '''
+            format color,print after this function will changed as arg style.
+        :param style: list of style,use TColor or number
+        :return: None
+        '''
         if not len(style):
             return
+
         method = -1
         foreground = -1
         background = -1
@@ -56,25 +76,53 @@ class TColorP:
             else:
                 print(f'\033[{method}m',end='')
 
-    def normal(self,txt:str,**args):
-        self.formatColor([TColor.method_normal])
+    def normal(self,txt:str,**args) -> None:
+        '''
+        print any text with normal style,default style is [method_normal].
+        :param txt: text you want to print
+        :param args: end arg like print(), style arg for change print style once
+        :return: None
+        '''
+        style = args.get('style',self.normalStyle)
+        self.formatColor(style)
         end = args.get('end', '\n')
         print(txt, end=end)
 
-    def success(self,txt:str,**args):
-        self.formatColor([TColor.method_normal,TColor.foreground_green])
+    def success(self,txt:str,**args) -> None:
+        '''
+        print any text with normal style and foreground is green,change once with style arg.
+        :param txt: text you want to print
+        :param args: end arg like print(),style arg for change print style once
+        :return: None
+        '''
+        style = args.get('style',[TColor.method_normal,TColor.foreground_green])
+        self.formatColor(style)
         end = args.get('end', '\n')
         print(txt, end=end)
         self.formatColor([TColor.method_normal])
 
-    def error(self,txt:str,**args):
-        self.formatColor([TColor.method_normal,TColor.foreground_red])
+    def error(self,txt:str,**args) -> None:
+        '''
+        print any text with normal style and foreground is red,change once with style arg
+        :param txt: text you want to print
+        :param args: end arg like print(),style arg for change print style once
+        :return: None
+        '''
+        style = args.get('style',[TColor.method_normal,TColor.foreground_red])
+        self.formatColor(style)
         end = args.get('end', '\n')
         print(txt, end=end)
         self.formatColor([TColor.method_normal])
 
-    def warning(self,txt:str,**args):
-        self.formatColor([TColor.method_normal,TColor.foreground_yellow])
+    def warning(self,txt:str,**args) -> None:
+        '''
+        print any text with normal style and foreground is yellow,change once with style arg
+        :param txt: text you want to print
+        :param args: end arg like print(),style arg for change print style once
+        :return: None
+        '''
+        style = args.get('style',[TColor.method_normal,TColor.foreground_yellow])
+        self.formatColor(style)
         end = args.get('end','\n')
         print(txt,end=end)
         self.formatColor([TColor.method_normal])
